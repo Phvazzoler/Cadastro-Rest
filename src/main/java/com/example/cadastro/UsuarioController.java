e5035901e2dfaecd6d1c81e3d186da1afd3eb8f4
PEDRO HENRIQUE CRUZ VAZZOLER <pedro.vazzoler@senaisp.edu.br>
2026-08-17 10:14:15 -0300

    Initial commit

---
+++ src/main/java/com/example/cadastro/UsuarioController.java
@@ -0,0 +1,23 @@
+
+import com.example.cadastro.Usuario;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+    ArrayList<Usuario> usuarios = new ArrayList<>();
+
+    @GetMapping
+    public ArrayList<Usuario> listarTodosUsuarios(){
+        return usuarios;
+    }
+    @PostMapping
+    public Usuario cadastrarUsuario(@RequestBody Usuario usuario){
+        usuarios.add(usuario);
+        return usuarios.getLast();
+    }
+}
+
